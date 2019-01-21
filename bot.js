732c2fda227e323c03970e2436ea14e2cb8b684e
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-21 21:14:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Jack' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=bc |3bker Broadcast`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Master Server`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
